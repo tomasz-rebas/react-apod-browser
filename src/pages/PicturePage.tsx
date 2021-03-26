550d4625a3bef619ae64a80bacfb9ae96f870269
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-03-26 19:47:01 +0100

    Styles for PicturePage.tsx

--- src/pages/PicturePage.tsx
+++ src/pages/PicturePage.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/css';
 import { Link } from 'react-router-dom';
 
 type Props = {
@@ -8,12 +9,46 @@ type Props = {
 
 export default function PicturePage({ data }: Props) {
 
+    const styleWrapper = css`
+        display: flex;
+        justify-content: center;
+    `;
+
+    const styleMain = css`
+        max-width: 700px;
+    `;
+
+    const styleBack = css`
+        color: #ddd;
+    ;`;
+
+    const styleImage = css`
+        max-width: inherit;
+        max-height: 700px;
+    `;
+
+    const styleParagraph = css`
+        text-align: justify;
+        font-size: 1.2em;
+    `;
+
     return (
         <div>
             <Link to="/">
-                <h3>&#60; Back</h3>
+                <h3 className={styleBack}>&#60; Back</h3>
             </Link>
-            <h1>{data.title}</h1>
+            <div className={styleWrapper}>   
+                <main className={styleMain}>
+                    <h1>{data.title}</h1>
+                    <img 
+                        className={styleImage}
+                        src={data.url}
+                        alt="APOD"
+                    />
+                    <p className={styleParagraph}>{data.explanation}</p>
+                    <p>The photo taken by {data.copyright}</p>
+                </main>
+            </div>
         </div>
     )
 }
