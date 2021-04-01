d2665a9821217fe5dbfb661b3b31dcad73242b5f
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-04-01 11:26:24 +0200

    Update Start.tsx

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -19,8 +19,24 @@ export default function Start() {
         console.log(inputDateRanges);
     }, [inputDateRanges]);
 
+    const style = {
+        wrapper: css`
+            text-align: center;
+        `
+    }
+
     return (
-        <div>
+        <div className={style.wrapper}>
+            <h3>Welcome to</h3>
+            <h1>Astronomy Picture of the Day Browser</h1>
+            <p>
+                The browser will display a list of Astronomy Pictures of the Day from NASA's archive based on your selected time.
+                <br/>
+                Select your desired time and click Continue.
+                <br/>
+                <br/>
+                In case you're not familiar with Astronomy Picture of the Day go check it out <a href="https://apod.nasa.gov/apod/astropix.html">here</a>.
+            </p>
             <DateRangePicker
                 ranges={inputDateRanges}
                 months={2}
