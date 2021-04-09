381a1ef852fe15dcae95cd5269582a8c716a0425
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-04-09 18:40:14 +0200

    Link for going back to date range selection

--- src/pages/Grid.tsx
+++ src/pages/Grid.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/css';
 import Tile from '../components/Tile';
+import { Link } from 'react-router-dom';
 
 type Props = {
     data: Array<{[key: string]: string}>
@@ -23,8 +24,13 @@ export default function Grid({ data }: Props) {
     `;
 
     return (
-        <div className={style}>
-            {data ? tiles : ''}
+        <div>
+            <Link to="/">
+                <h3>&#60; Choose different date range</h3>
+            </Link>
+            <div className={style}>
+                {data ? tiles : ''}
+            </div>
         </div>
     );
 }
