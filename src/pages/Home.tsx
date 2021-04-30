15b0d03c60c81562df2611ea61bdd0bdf4a3dc26
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-04-30 12:25:00 +0200

    Define grid media queries statically (it performs better)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -48,7 +48,31 @@ export default function Home({ data, fetchError, setDarkMode }: Props) {
         grid: css`
             margin-top: 10px;
             display: grid;
-            ${generateGridMediaQueries(200, 2560)}
+
+            @media (max-width: 300px) {
+                grid-template-columns: 1fr
+            }
+            @media (min-width: 301px) and (max-width: 600px) {
+                grid-template-columns: repeat(2, 1fr)
+            }
+            @media (min-width: 601px) and (max-width: 900px) {
+                grid-template-columns: repeat(3, 1fr)
+            }
+            @media (min-width: 901px) and (max-width: 1200px) {
+                grid-template-columns: repeat(4, 1fr)
+            }
+            @media (min-width: 1201px) and (max-width: 1500px) {
+                grid-template-columns: repeat(5, 1fr)
+            }
+            @media (min-width: 1501px) and (max-width: 1800px) {
+                grid-template-columns: repeat(6, 1fr)
+            }
+            @media (min-width: 1801px) and (max-width: 2100px) {
+                grid-template-columns: repeat(7, 1fr)
+            }
+            @media (min-width: 2101px) {
+                grid-template-columns: repeat(8, 1fr)
+            }
         `
     };
 
