77cac90c96c1732b621f049db43057abbee885a0
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2021-04-30 17:49:55 +0200

    Article's content gets its own component

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,8 +1,8 @@
 import { css } from '@emotion/css';
 import { Link } from 'react-router-dom';
-import getDateString from '../functions/getDateString';
 import { useEffect, useContext } from 'react';
 import { ThemeContext } from '../components/Routes';
+import Article from '../components/Article';
 
 type Props = {
     data: {
@@ -23,21 +23,6 @@ export default function ArticlePage({ data, adjacentArticles }: Props) {
     }, []);
 
     const style = {
-        wrapper: css`
-            display: flex;
-            justify-content: center;
-            margin-left: 10px;
-            margin-right: 10px;
-        `,
-        main: css`
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            max-width: 700px;
-        `,
-        header: css`
-            text-align: center;
-        `,
         linkWrapper: css`
             &:first-child {
                 flex: 1;
@@ -64,41 +49,6 @@ export default function ArticlePage({ data, adjacentArticles }: Props) {
                 }
             }
         ;`,
-        picture: css`
-            opacity: 0;
-            object-fit: contain;
-            width: auto;
-            height: auto;
-            max-width: 100%;
-            max-height: 100%;
-        `,
-        pictureWrapper: css`
-            max-width: inherit;
-            max-height: 500px;
-            text-align: center;
-            background: url(${data.url}) center center no-repeat;
-            background-size: contain;
-        `,
-        iframeWrapper: css`
-            text-align: center;
-        `,
-        iframe: css`
-            width: 560px;
-            height: 315px;
-            
-            @media (max-width: 600px) {
-                width: 300px;
-                height: 240px;
-            }
-        `,
-        p: css`
-            text-align: justify;
-            font-size: 1.2em;
-
-            @media (max-width: 640px) {
-                text-align: left;
-            }
-        `,
         navigation: css`
             display: flex;
             justify-content: space-between;
@@ -143,40 +93,9 @@ export default function ArticlePage({ data, adjacentArticles }: Props) {
                 }
                 </div>
             </nav>
-            <div className={style.wrapper}>   
-                <main className={style.main}>
-                    <h1 className={style.header}>
-                        {data.title}
-                    </h1>
-                    <h3 className={style.header}>
-                        {getDateString(data.date, 'D Month Yr')}
-                    </h3>
-                    {
-                        data.media_type === 'image' ?
-                        <div className={style.pictureWrapper}>
-                            <img 
-                                className={style.picture}
-                                src={data.url}
-                                alt="APOD"
-                            />
-                        </div> :
-                        <div className={style.iframeWrapper}>
-                            <iframe 
-                                className={style.iframe}
-                                src={data.url}
-                                frameBorder="0"
-                                allow="autoplay; encrypted-media"
-                                allowFullScreen
-                                title={data.title}
-                            >
-                            </iframe>
-                        </div>
-                    }
-                    
-                    <p className={style.p}>{data.explanation}</p>
-                    {data.copyright ? <p>The photo taken by {data.copyright}</p> : ''}
-                </main>
-            </div>
+            <main>
+                <Article data={data}/>
+            </main>
         </div>
     )
 }
